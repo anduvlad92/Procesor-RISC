0f259221e8af8e4282bb7ab642463deebad21686
Vlad Andrei <vladandrei92@gmail.com>
2014-10-26 14:25:49 +0200

    Added file register

--- program_memory.v
+++ program_memory.v
@@ -3,10 +3,12 @@ input reset,
 input clock,
 output reg [31:0] data);
 reg [31:0] program_memory[15:0];
+integer unsigned i;
 	always@(posedge clock) begin
 		if(reset)
-			for(i=0;i<65535;i=i+1)
+			for(i=0;i<=65535;i=i+1)
 				program_memory[i]<=0;
+			
 		else	
 			data<=program_memory[addres];
 	end
