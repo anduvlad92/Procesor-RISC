1476b211aa3c39604d475d19b9bef81c5a8b946f
Vlad Andrei <vladandrei92@gmail.com>
2014-10-29 18:04:42 +0200

    tweaks

--- data_memory.v
+++ data_memory.v
@@ -4,12 +4,12 @@ module data_memory(input [31:0] addres,
 	input we,
 	input [31:0] data_in,
 	output reg[31:0] data_out);
-reg [31:0] data_memory[31:0];
+reg [31:0] data_memory[0:4294967295];
 integer unsigned  i;
 	always@(posedge clock) begin
 		if(reset)
 			for(i=0;i<=4294967295;i=i+1)
-				data_memory[i]<=0;
+				data_memory[i]<=32'h00000000;
 			
 		else
 			if(we)
--- program_memory.v
+++ program_memory.v
@@ -2,12 +2,12 @@ module program_memory(input [15:0] addres,
 input reset,
 input clock,
 output reg [31:0] data);
-reg [31:0] program_memory[15:0];
+reg [31:0] program_memory[0:65535];
 integer unsigned i;
 	always@(posedge clock) begin
 		if(reset)
 			for(i=0;i<=65535;i=i+1)
-				program_memory[i]<=0;
+				program_memory[i]<=32'h00000000;
 		else	
 			data<=program_memory[addres];
 	end
